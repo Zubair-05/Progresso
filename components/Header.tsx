c9600ac066d8333fef82b16887a7bf4ffa7ec53a
Zubair-05 <106993633+Zubair-05@users.noreply.github.com>
2023-07-23 23:19:33 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -63,7 +63,7 @@ const Header = () => {
                 <p className="flex items-center p-3 text-sm font-normal pr-5 my-3 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
                     <UserCircleIcon className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${loading && "animate-spin"}`} />
                     {
-                        suggestion && !loading ? suggestion : `Summarising your tasks for the day...`
+                        suggestion 
                     }
                 </p>
             </div>
